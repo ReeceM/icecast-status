c9ba77bb63bbf8f36a1db0ab2d6664ab1d8dec89
ReeceM <reecestevenmay@gmail.com>
2020-06-02 01:11:59 +0200

    configs issues

--- postcss.config.js
+++ postcss.config.js
@@ -5,7 +5,7 @@ const purgecss = require('@fullhuman/postcss-purgecss')({
 
     // Specify the paths to all of the template files in your project
     content: [
-        "./public/**/*.html",
+        "./src/**/*.html",
         "./src/**/*.js",
     ],
 
