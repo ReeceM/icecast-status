0f213c190e5cae31cd2f3c2fb398a6eb4bb0f307
ReeceM <reecestevenmay@gmail.com>
2020-06-12 20:18:54 +0200

    try catch data

--- src/lambda/proxy-status.js
+++ src/lambda/proxy-status.js
@@ -1,20 +1,25 @@
 const axios = require('axios');
 
 export async function handler(event, context) {
-	try {
-		let decoded = JSON.parse(event.body);
-		let requestedUrl = decoded.source;
+	console.log(event);
 
-		const response = await axios.get(requestedUrl);
-
-		return {
-			statusCode: 200,
-			body: JSON.stringify(response.json())
-		}
-	} catch (error) {
-		return {
-			statusCode: error.statusCode,
-			body: error
-		}
+	return {
+		statusCode: 200,
+		body: JSON.stringify({hello: "World"})
 	}
+	// try {
+	// 	let decoded = JSON.parse(event.body);
+	// 	let requestedUrl = decoded.source;
+	// 	const response = await axios.get(requestedUrl);
+
+	// 	return {
+	// 		statusCode: 200,
+	// 		body: JSON.stringify(response.json())
+	// 	}
+	// } catch (error) {
+	// 	return {
+	// 		statusCode: error.statusCode,
+	// 		body: JSON.stringify(error)
+	// 	}
+	// }
 }
